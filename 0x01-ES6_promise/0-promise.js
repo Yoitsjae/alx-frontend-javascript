35e74a41d8558a641e107c55b46348fae1c2cb8d
Yoitsjae <ncubejason896@gmail.com>
2024-03-06 19:23:29 +0200

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,9 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    // Perform some asynchronous operation here
-    // For demonstration purposes, resolve with a dummy response after a timeout
-    setTimeout(() => {
-      resolve("Dummy API response");
-    }, 1000);
+      // Simulating API call
+      setTimeout(() => {
+          // Assuming successful response
+          resolve("Successful response from API");
+      }, 2000); // Simulating delay of 2 seconds
   });
 }
