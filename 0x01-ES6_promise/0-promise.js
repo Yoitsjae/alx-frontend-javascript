b44caae1aac3bbc55cccab0f5edd6c2dadb66ddd
Yoitsjae <ncubejason896@gmail.com>
2024-03-08 11:57:55 +0200

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,14 +1,3 @@
-// Define a function named getFullResponseFromAPI
-// It returns a promise that resolves with a success object if success is true, otherwise rejects with an error message
-const getFullResponseFromAPI = (success) => {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' }); // Resolve with success object
-    } else {
-      reject(Error('Sorry, the API is currently unavailable')); // Reject with error message
-    }
-  });
-};
-
-// Export the getFullResponseFromAPI function as default
-export default getFullResponseFromAPI;
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
+}
