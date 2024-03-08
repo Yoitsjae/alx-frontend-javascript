37bb5a8b90824e9284a195b4a2f3e9a3fbe57319
Yoitsjae <ncubejason896@gmail.com>
2024-03-08 11:47:27 +0200

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,8 @@
-export default function getFullResponseFromAPI(success) {
+function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
+    if (success) resolve({ status: 200, body: 'Success' });
+    reject(Error('fake API is not working '));
   });
 }
 
+export default getFullResponseFromAPI;
